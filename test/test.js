1e169e44c7dcf484f059fc3672458972a8a964c6
Cyrus Eslami <ceslami@gmail.com>
2014-04-26 23:24:40 -0400

    Add carrier#isValidDomain to make sure passed-in carrierDomains are supported

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require("assert"),
+    textmail = require("../lib/mailer.js");
+
+describe('textmail', function(){
+  describe('isClosed', function(){
+    it('should be open immediately after being intialized', function(){
+      var texter = textmail.textmail();
+      assert.equal(texter.isClosed, false);
+    })
+  })
+})
