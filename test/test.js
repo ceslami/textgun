2461121e4865ac9f4465e2bab554c25e04c0c300
Cyrus Eslami <ceslami@gmail.com>
2014-04-27 01:16:45 -0400

    Add watch-test script, add test for @tranport field, reformat status priting for watch-lint

--- test/test.js
+++ test/test.js
@@ -1,11 +1,19 @@
 var assert = require("assert"),
     textmail = require("../lib/mailer.js");
 
-describe('textmail', function(){
-  describe('isClosed', function(){
-    it('should be open immediately after being intialized', function(){
-      var texter = textmail.textmail();
-      assert.equal(texter.isClosed, false);
+describe('textmail', function() {
+
+    describe('@isClosed', function() {
+        it('should be open immediately after being intialized', function() {
+            var texter = textmail.textmail();
+            assert.equal(texter.isClosed, false);
+        });
     });
-  });
+
+    describe('@transport', function() {
+        it('should have a transport protool configured', function() {
+            var texter = textmail.textmail();
+            assert.notEqual(texter.transport, null);
+        });
+    });
 });
