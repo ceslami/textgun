e0c2a44552857792a1e5ddbe2406cf087aab4e40
Cyrus Eslami <ceslami@gmail.com>
2014-04-29 02:44:51 -0400

    Simply logic around constraint of only using simple SMTP connections for senders. Updated tests.

--- test/mailer.js
+++ test/mailer.js
@@ -2,21 +2,11 @@ var assert = require('assert'),
     textmail = require('../lib/mailer.js');
 
 describe('textmail', function() {
-
-    describe('@isClosed', function() {
-        it('should be open immediately after being intialized', function() {
-            var text = textmail();
-            assert.equal(text.isClosed, false);
-        });
-    });
-
     describe('@transport', function() {
         it('should have a transport protocol configured', function() {
             var text = textmail({
-                from: {
-                    user: 'admin@example.com',
-                    pass: 'password'
-                }
+                username: 'admin@example.com',
+                password: 'password'
             });
             assert.notEqual(text.transport, null);
         });
