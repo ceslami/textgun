68bfdb8507e7e074fbf069b3735b07939b602af6
Cyrus Eslami <ceslami@gmail.com>
2014-05-04 16:43:53 -0400

    Added tests for validating numbers in the two field

--- test/api.js
+++ test/api.js
@@ -22,7 +22,7 @@ describe('textmail', function() {
             }
         });
 
-        it('throws an error if empty config object is passed', function() {
+        it('throws if empty config object is passed', function() {
             try {
                 textmail({});
             } catch(e) {
@@ -31,7 +31,7 @@ describe('textmail', function() {
             }
         });
 
-        it('throws an error if there is no username field', function() {
+        it('throws if there is no username field', function() {
             try {
                 textmail({
                     password: '***'
@@ -40,6 +40,57 @@ describe('textmail', function() {
                 assert(e.message === 'SMTP sender options do not include a ' +
                     'username. Typically this is your email address.');
             }
+        });
+    });
+
+    describe('Message sending options', function() {
+        it('throws when phone number is not 10 digits', function() {
+            try {
+                textmail({
+                    username: 'example',
+                    password: '***'
+                }).send({
+                    to: '2031',
+                    text: 'hi',
+                    carrierDomain: 'txt.att.net'
+                });
+            } catch(e) {
+                assert(e.message === 'Phone number must be a purely numberic ' +
+                '10 character string without letters or special characters.');
+            }
+        });
+
+        it('throws if the phone number isnt purely alphanumeric', function() {
+            try {
+                textmail({
+                    username: 'example',
+                    password: '***'
+                }).send({
+                    to: '20asd31asd',
+                    text: 'hi',
+                    carrierDomain: 'txt.att.net'
+                });
+            } catch(e) {
+                assert(e.message === 'Phone number must be a purely numberic ' +
+                '10 character string without letters or special characters.');
+            }
+        });
+
+        it('succeeds if phone number is 10 digits and numeric', function() {
+            try {
+                textmail({
+                    username: 'example',
+                    password: '***'
+                }).send({
+                    to: '1234567890',
+                    text: 'hi',
+                    carrierDomain: 'txt.att.net'
+                });
+            } catch(e) {
+                assert(false);
+            }
+
+            assert(true);
         });
     });
 });
